93a7a1c8b8899d394ce9a06aa079b99d96690d4f
Rene Zhang <rz99@cornell.edu>
2014-09-11 23:35:46 -0400

    Added empty test file

--- src/queue_test.c
+++ src/queue_test.c
@@ -2,8 +2,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+void test_new() {
+
+}
+
+void test_prepend() {
+
+}
+
+void test_append() {
+
+}
+
+void test_dequeue() {
+
+}
+
+void test_length() {
+
+}
+
+void test_iterate() {
+
+}
+
+void test_free() {
+
+}
+
+void test_delete() {
+
+}
 
 int main(void) {
-    printf("Hello World!\n");
+    test_new();
+    test_prepend();
+    test_append();
+    test_dequeue();
+    test_length();
+    test_iterate();
+    test_free();
+    test_delete();
+
+    printf("All Tests Pass!!!\n");
     return 0;
 }
