2118984313af573005308f99c846b62cd7737648
Rene Zhang <rz99@cornell.edu>
2014-09-21 23:49:43 -0400

    cleaned up shop a bit

--- src/shop.c
+++ src/shop.c
@@ -1,12 +1,9 @@
 /*
- * TODO: recomment
- * Bounded buffer example. 
+ * This program implements a retail store in which there are employees
+ * and customers. The employees act as producers and constantly unbox
+ * new phones, while the customers act as consumers and buy phones.
  *
- * Sample program that implements a single producer-single consumer
- * system. To be used to test the correctness of the threading and
- * synchronization implementations.
- * 
- * Change MAXCOUNT to vary the number of items produced by the producer.
+ * Change the macros EMPLOYEES and CONSUMERS to modify the number of each.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,49 +11,57 @@
 #include "synch.h"
 
 #define EMPLOYEES 10
-#define CONSUMERS 1000
+#define CUSTOMERS 1000
 
-#define BUFFER 100
+int sold_id; // We will assume that ids are 1 indexed
+//int unpack_id;
+semaphore_t phones;
 
-int unpack, sell;
-
-semaphore_t empty;
-int consumer(int* arg) {
-    semaphore_P(empty);
-    sell += 1;
-    printf("Phone %i\n", sell);
+/*
+ * The consumer thread. They take a single phone and then print its id.
+ */
+int customer(int* arg) {
+    semaphore_P(phones);
+    sold_id += 1;
+    printf("Bought phone %i\n", sold_id);
 
     return 0;
 }
 
+/*
+ * The producer thread. They constantly unbox new phones.
+ */
 int employee(int* arg) {
     while (1) {
-        unpack += 1;
-        //printf("Unpacked phone %i\n", unpack);
-        semaphore_V(empty);
+        //unpack_id += 1;
+        //printf("Unpacked phone %i\n", unpack_id);
+        semaphore_V(phones);
         minithread_yield();
     }
 
-    return -1;
+    return -1; // This thread should never end
 }
 
+/*
+ * A starting thread to spawn all of the employees and customers.
+ */
 int spawner(int *arg) {
     int i;
     for (i = 0; i < EMPLOYEES; i++) {
         minithread_fork(employee, NULL);
     }
-    for (i = 0; i < CONSUMERS; i++) {
-        minithread_fork(consumer, NULL);
+    for (i = 0; i < CUSTOMERS; i++) {
+        minithread_fork(customer, NULL);
     }
     return 0;
 }
 
 int
 main(int argc, char * argv[]) {
-    empty = semaphore_create();
-    semaphore_initialize(empty, 0);
-    unpack = 0;
-    sell = 0;
+    phones = semaphore_create();
+    semaphore_initialize(phones, 0);
+    sold_id = 0;
+    //unpack_id = 0;
 
     minithread_system_initialize(spawner, NULL);
     return -1;
