f80e227a23216a31727c75a98b802a8cedd2512a
fukangwen <fukangwen@gmail.com>
2014-11-20 19:37:47 -0800

    spacing

--- src/cache.c
+++ src/cache.c
@@ -76,25 +76,25 @@ hashtable_t hashtable_new(int size) {
 }
 
 //void compare_keys(void* tuple, void* key, void** output) {
-//	tuple_t t;
-//	network_address_t addr;
-//	t = (tuple_t) tuple;
-//	addr = (unsigned int *) key;
+//  tuple_t t;
+//  network_address_t addr;
+//  t = (tuple_t) tuple;
+//  addr = (unsigned int *) key;
 //      addr = (network_address_t) addr;
-//	if (network_compare_network_addresses(addr, t->key) != 0) {
-//		*output = tuple;
-//	}
+//  if (network_compare_network_addresses(addr, t->key) != 0) {
+//      *output = tuple;
+//  }
 //}
 
 int hash_naive(network_address_t address, int size) {
-	unsigned short unmodded;
-	int result;
+    unsigned short unmodded;
+    int result;
 
-	unmodded = hash_address(address);
+    unmodded = hash_address(address);
 
-	result = ((int) unmodded) % (2 * size);
+    result = ((int) unmodded) % (2 * size);
 
-	return result;
+    return result;
 }
 /*
  * returns the tuple of k,v,l,c pair if hashtable contains the specified key,
@@ -112,10 +112,10 @@ tuple_t hashtable_contains(hashtable_t hashtable, network_address_t key) {
   //  input = (void *) key;
   //  list_iterate(hashtable->buckets[hashed_key], compare_keys, input, output);
   //  if ((*output) != NULL) {
-  //  	result = (tuple_t) (*output);
-  //  	return result;
+  //    result = (tuple_t) (*output);
+  //    return result;
   //  } else {
-  //  	return NULL;
+  //    return NULL;
   //  }
 
      checked = 0;
@@ -161,7 +161,7 @@ void * hashtable_set(hashtable_t hashtable, tuple_t tup) {
     found_kv = hashtable_contains(hashtable, tup->key);
     if (!found_kv) {
         //hashed_key = hash(key);
-    	node = list_append(hashtable->buckets[hashed_key], tup);
+        node = list_append(hashtable->buckets[hashed_key], tup);
         if (node == NULL) return NULL;
         return node;
     } else {
@@ -203,131 +203,131 @@ int hashtable_free (hashtable_t hashtable) {
  * Return an empty cache of some specified size.  Returns NULL on error.
  */
 cache_t cache_new(int size) {
-	hashtable_t h;
-	list_t l;
-	cache_t c;
+    hashtable_t h;
+    list_t l;
+    cache_t c;
 
-	c = (cache_t) malloc (sizeof(struct cache));
-	l = list_new();
-	h = hashtable_new(size);
+    c = (cache_t) malloc (sizeof(struct cache));
+    l = list_new();
+    h = hashtable_new(size);
 
-	if (!l || !c || !h) {
-		free(l);
-		free(c);
-		free(h);
-		return NULL;
-	}
+    if (!l || !c || !h) {
+        free(l);
+        free(c);
+        free(h);
+        return NULL;
+    }
 
-	c->list = l;
-	c->hashtable = h;
-	c->current_size = 0;
-	c->max_size = size;
-	return c;
+    c->list = l;
+    c->hashtable = h;
+    c->current_size = 0;
+    c->max_size = size;
+    return c;
 }
 
 /*
  * Set the specified key as this value. Returns 0 (success) or -1 (failure)
  */
 int cache_set(cache_t cache, network_address_t key, void *value, void** output) {
-	void *result;
-	tuple_t tup;
-	tuple_t evicted;
-	void *l_node;
-	void *c_node;
+    void *result;
+    tuple_t tup;
+    tuple_t evicted;
+    void *l_node;
+    void *c_node;
     if (!cache || !key || !output) return -1;
-	if (hashtable_get(cache->hashtable, key, &result) == -1) {
+    if (hashtable_get(cache->hashtable, key, &result) == -1) {
 
-		tup = (tuple_t) malloc (sizeof(struct tuple));
-		if (!tup) return -1;
-		network_address_copy(key, tup->key);
-		tup->value = value;
+        tup = (tuple_t) malloc (sizeof(struct tuple));
+        if (!tup) return -1;
+        network_address_copy(key, tup->key);
+        tup->value = value;
 
-		if (cache->current_size < cache->max_size) {
-			l_node = list_append(cache->list, tup);
-			if (!l_node) return -1;
-			tup->list_node = l_node;
-			c_node = hashtable_set(cache->hashtable, tup);
-			if (!c_node) return -1;
-			tup->chain_node = c_node;
-			cache->current_size++;
-                        *output = NULL;
-			return 0;
-		} else {
-			if (list_dequeue(cache->list, &result) == 0) {
-				evicted = (tuple_t) result;
-                                *output = evicted->value;
-				hashtable_delete(cache->hashtable, evicted);
-				free(evicted);
-				l_node = list_append(cache->list, tup);
-				if (!l_node) return -1;
-				tup->list_node = l_node;
-				c_node = hashtable_set(cache->hashtable, tup);
-				if (!c_node) return -1;
-				tup->chain_node = c_node;
-				return 0;
-			} else {
-                                *output = NULL;
-				return -1;
-			}
-		}
-	} else {
-		tup = (tuple_t) result;
-		tup->value = value;
+        if (cache->current_size < cache->max_size) {
+            l_node = list_append(cache->list, tup);
+            if (!l_node) return -1;
+            tup->list_node = l_node;
+            c_node = hashtable_set(cache->hashtable, tup);
+            if (!c_node) return -1;
+            tup->chain_node = c_node;
+            cache->current_size++;
+            *output = NULL;
+            return 0;
+        } else {
+            if (list_dequeue(cache->list, &result) == 0) {
+                evicted = (tuple_t) result;
+                *output = evicted->value;
+                hashtable_delete(cache->hashtable, evicted);
+                free(evicted);
+                l_node = list_append(cache->list, tup);
+                if (!l_node) return -1;
+                tup->list_node = l_node;
+                c_node = hashtable_set(cache->hashtable, tup);
+                if (!c_node) return -1;
+                tup->chain_node = c_node;
+                return 0;
+            } else {
                 *output = NULL;
-		return 0;
-	}
+                return -1;
+            }
+        }
+    } else {
+        tup = (tuple_t) result;
+        tup->value = value;
+        *output = NULL;
+        return 0;
+    }
 }
 
 /*
  * get the value of the specified key. Returns 0 (success) or -1 (failure)
  */
 int cache_get(cache_t cache, network_address_t key, void ** output) {
-	void *result;
-	tuple_t tup;
+    void *result;
+    tuple_t tup;
     if (!cache || !key || !output) return -1;
-	if (hashtable_get(cache->hashtable, key, &result) == 0) {
-		tup = (tuple_t) result;
-		*output = tup->value;
-		return 0;
-	}
-	return -1;
+    if (hashtable_get(cache->hashtable, key, &result) == 0) {
+        tup = (tuple_t) result;
+        *output = tup->value;
+        return 0;
+    }
+    return -1;
 }
 
 /*
  * delete the corresponding key from the cache. Returns 0 (success) or -1 (failure)
  */
 int cache_delete(cache_t cache, network_address_t key) {
-	void *result;
-	tuple_t tup;
+    void *result;
+    tuple_t tup;
     if (!cache || !key) return -1;
-	if (hashtable_get(cache->hashtable, key, &result) == 0) {
-		tup = (tuple_t) result;
-		cache->current_size --;
-		if (hashtable_delete(cache->hashtable, tup) == -1) return -1;
-		if (list_delete(cache->list, tup->list_node) == -1) return -1;
-		free(tup);
-		return 0;
-	}
-	return -1;
+    if (hashtable_get(cache->hashtable, key, &result) == 0) {
+        tup = (tuple_t) result;
+        cache->current_size --;
+        if (hashtable_delete(cache->hashtable, tup) == -1) return -1;
+        if (list_delete(cache->list, tup->list_node) == -1) return -1;
+        free(tup);
+        return 0;
+    }
+    return -1;
 }
 
 /*
  * Free the cache and return 0 (success) or -1 (failure).
  */
 int cache_destroy (cache_t cache) {
-	void *result;
-	tuple_t tup;
+    void *result;
+    tuple_t tup;
 
     if (!cache) return -1;
-	while (list_dequeue(cache->list, &result) == 0) {
-		tup = (tuple_t) result;
-		if (list_delete(cache->list, tup->list_node) == -1) return -1;
-		free(tup);
-	}
-	if (list_free(cache->list) == -1) return -1;
+    while (list_dequeue(cache->list, &result) == 0) {
+        tup = (tuple_t) result;
+        if (list_delete(cache->list, tup->list_node) == -1) return -1;
+        free(tup);
+    }
+    if (list_free(cache->list) == -1) return -1;
 
-	if (hashtable_free(cache->hashtable) == -1) return -1;
-	free(cache);
-	return 0;
+    if (hashtable_free(cache->hashtable) == -1) return -1;
+    free(cache);
+    return 0;
 
 }
