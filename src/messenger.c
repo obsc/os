7d2c1054c88f9e50e4a58f4a996100e1feaa7f34
Fukang Wen <fukangwen@gmail.com>
2014-11-22 00:25:46 -0500

    mes

--- src/messenger.c
+++ src/messenger.c
@@ -11,8 +11,8 @@
 
 char* hostname;
 int port = 80; /* port on which we do the communication */
-int version;
-minisocket_t socket;
+int version_num;
+minisocket_t socket_glob;
 
 int receiver(int *arg) {
   char buffer[BUFFER_SIZE];
@@ -21,10 +21,10 @@ int receiver(int *arg) {
   int i;
 
   while (1) {
-    len = minisocket_receive(socket,buffer, BUFFER_SIZE, &error);
+    len = minisocket_receive(socket_glob,buffer, BUFFER_SIZE, &error);
     if (len == -1) {
       printf("Receiving error. Code: %d.\n", error);
-      minisocket_close(socket);
+      minisocket_close(socket_glob);
       return 0;
     } else {
       for (i = 0; i < len; i++) {
@@ -42,15 +42,15 @@ int sender(int* arg) {
   network_address_t address;
   minisocket_error error;
 
-  if (version == 2) {
-    socket = minisocket_server_create(port,&error);
+  if (version_num == 2) {
+    socket_glob = minisocket_server_create(port,&error);
   } else {
     network_translate_hostname(hostname, address);
     /* create a network connection to the remote machine */
-    socket = minisocket_client_create(address, port,&error);
+    socket_glob = minisocket_client_create(address, port,&error);
   }
 
-  if (socket==NULL){
+  if (socket_glob==NULL){
     printf("Can't create the server. Error code: %d.\n",error);
     return 0;
   }
@@ -62,12 +62,12 @@ int sender(int* arg) {
     bytes_sent=0;
     while (bytes_sent!=len){
       int trans_bytes=
-        minisocket_send(socket,buffer+bytes_sent,
+        minisocket_send(socket_glob,buffer+bytes_sent,
             len-bytes_sent, &error);
       //printf("Sent %d bytes.\n", trans_bytes);
       if (trans_bytes==-1){
         printf("Sending error. Code: %d.\n", error);
-        minisocket_close(socket);
+        minisocket_close(socket_glob);
         return 0;
       }
       bytes_sent+=trans_bytes;
@@ -80,11 +80,11 @@ int main(int argc, char** argv) {
 
   if (argc > 1) {
       hostname = argv[1];
-      version = 1;
+      version_num = 1;
       minithread_system_initialize(sender, NULL);
   }
   else {
-      version = 2;
+      version_num = 2;
       minithread_system_initialize(sender, NULL);
   }
   return -1;
