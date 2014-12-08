21488815a1e54c19209ff61ef1ca6bd27b508db9
Rene Zhang <rz99@cornell.edu>
2014-12-08 02:31:01 -0500

    compile errors on fsck

--- src/fsck.c
+++ src/fsck.c
@@ -5,20 +5,20 @@
 #include <string.h>
 #include "disk.h"
 #include "minifile.h"
+#include "miniheader.h"
 
 int size;
 int max_inode_index;
 superblock_t sprblk;
 
 int root_inode_num;
-int first_free_inode;
-int first_free_data_block;
+int free_inode;
+int free_data_block;
 
 // Validates the superblock
 // Returns -1 on failure
 int validate_superblock() {
     waiting_request_t req;
-    int magic_number;
 
     req = read_block(0, (char *) sprblk);
     semaphore_P(req->wait);
@@ -29,19 +29,19 @@ int validate_superblock() {
     }
     free(req);
 
-    if (unpack_unsigned_int(sprblk->magic_number) != MAGIC) {
+    if (unpack_unsigned_int(sprblk->data.magic_number) != MAGIC) {
         printf("Invalid magic number in superblock\n");
         return -1;
     }
-    if (unpack_unsigned_int(sprblk->disk_size) != size) {
+    if (unpack_unsigned_int(sprblk->data.disk_size) != size) {
         printf("Invalid size in superblock\n");
         return -1;
     }
 
-    root_inode_num = unpack_unsigned_int(sprblk->root_inode);
-    first_free_inode = unpack_unsigned_int(sprblk->first_free_inode);
-    first_free_data_block = unpack_unsigned_int(sprblk->first_free_data_block);
-    
+    root_inode_num = unpack_unsigned_int(sprblk->data.root_inode);
+    free_inode = unpack_unsigned_int(sprblk->data.first_free_inode);
+    free_data_block = unpack_unsigned_int(sprblk->data.first_free_data_block);
+
     return 0;
 }
 
@@ -51,8 +51,11 @@ int fsck(int *arg) {
 
     sprblk = (superblock_t) malloc (sizeof (struct superblock));
 
+    printf("Validating superblock\n");
     if (validate_superblock() == -1) return -1;
 
+
+    printf("File system validated\n");
     return 0;
 }
 
